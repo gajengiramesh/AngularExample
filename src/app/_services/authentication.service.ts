c86dc6d5217d5b606475d08886fc809f8489b2ed
gajengi <gajengiramesh@gmail.com>
2018-12-20 22:39:21 -0500

    completed basic login authentication

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {User} from '../_models/user';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  service_url:string
 
-  constructor(private http: HttpClient) { }
-
-  login(loginName:string,loginPassowrd:string)
+  constructor(private http: HttpClient) { 
+    this.service_url = environment.service_url
+  }
+  login(loginName:string,loginPassowrd:string) :Observable<any>
   {
+    let url:string = `${this.service_url}auth/login/`
+     
+    console.log(url)
+    let output:Observable<Object> = this.http.post(url,{'user_name':loginName,'password':loginPassowrd})
+    return output.pipe(map(res => res))
+    // output.subscribe(val => { this.tt = val as Response
+    //                         },err => {console.log('error')
+    //                         console.log(err)},()=>{
+    //                           console.log('completed')
+    //                           console.log(this.tt)
+    //                           return this.tt
+    //                         })
+  }
+  login1(loginName:string,loginPassowrd:string)
+  {
+    let url:string = `${this.service_url}auth/login/${loginName}/${loginPassowrd}`
+    console.log(url)
+    let output:Observable<Object> = this.http.get(url)
+    output.subscribe(val => console.log(val))
+    console.log(output)
     if (loginName === 'Ramesh' && loginPassowrd ==='Ramesh') {
       // return new User({id=1,name='Ramesh',email='test@email.com'})
       return Object.assign(new User(),{id:1,name:'Ramesh',email:'test@email.com'})
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FormControl,Validators, FormGroup } from '@angular/forms';
 import {AuthenticationService} from '../_services/authentication.service'
+import { routerNgProbeToken } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +13,18 @@ export class LoginComponent implements OnInit {
   loginForm:FormGroup;
   loginNameControl:FormControl
   loginPasswordControl:FormControl
+  userId:Number
+  token:string
+  loginResponse:any
+  isSubmitted:boolean = false
 
 
-
-  constructor(private authService : AuthenticationService) { }
+  constructor(private authService : AuthenticationService,private router: Router) { 
+    console.log(this.userId)
+  }
 
   ngOnInit() {
+    console.log(this.userId)
     this.loginNameControl = new FormControl('',Validators.required);
     this.loginPasswordControl = new FormControl('',Validators.required);
     this.loginForm = new FormGroup({'loginNameControl':this.loginNameControl,'loginPasswordControl':this.loginPasswordControl})
@@ -25,17 +33,23 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     if(this.loginForm.valid){
       console.log("model-based form submitted");
-      var x =this.authService.login('Ramesh','Ramesh');
-      console.log(x);
-      console.log(this.loginNameControl.value);
-      console.log(this.loginNameControl.errors);
-      this.loginNameControl.setValue('testing') ;
+      this.isSubmitted = true // to diable the login button oterwise if login takes time user can click multiple times
+      let x = this.authService.login(this.loginNameControl.value,this.loginPasswordControl.value)
+      x.subscribe(loginResponse => this.loginResponse = loginResponse  , 
+                  err => { console.log(err)
+                        this.isSubmitted = false
+                        },
+                    ()=>{ console.log('completed') 
+                    localStorage.setItem('userId', this.loginResponse.user_id.toString())
+                    localStorage.setItem('token', this.loginResponse.token.toString())
+                    console.log(this.loginResponse)
+                    this.router.navigate(['home'])
+                  }) 
+
     }
     else{
-      console.log('bye');
-      this.loginNameControl.markAsTouched({ onlySelf: true })
+      this.loginNameControl.markAsTouched({ onlySelf: true }) //marking them dirty to display error message
       this.loginPasswordControl.markAsTouched({ onlySelf: true })
-      return ;
     }
 }
 
